77f9a897daf25a28e3615aef57b008ccad8bdf06
Tvs-2008 <subhashmsb2008@gmail.com>
2026-03-10 08:25:05 +0530

    coded to print oops using aarays and loops

--- OopsBannerApp.java
+++ OopsBannerApp.java
@@ -1,5 +1,18 @@
 public class OopsBannerApp{
     public static void main(String[] args){
-        System.out.println("OOPS");
+        String[] banner = {
+        "   ***           ***        ******            ******  ",
+        " **   **       **   **      **    **        **        ",
+        "**     **     **     **     **     **      **         ",
+        "**     **     **     **     **    **        **        ",
+        "**     **     **     **     ******            ***     ",
+        "**     **     **     **     **                  **   ",
+        "**     **     **     **     **                    **  ",
+        " **   **       **   **      **                    **  ",
+        "   ***           ***        **              *****     "
+    };
+        for(String line : banner){
+            System.out.println(line);
+        }
     }
 }
